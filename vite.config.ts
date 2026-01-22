c5469dfc4a36bbee8ed0ba43c5036dd836a79516
Fellype Queiroz <fellypequeirozmail@gmail.com>
2026-01-22 17:43:16 -0300

    configuração para vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-<<<<<<< HEAD
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
-=======
-  const env = loadEnv(mode, '.', '');
-  return {
-    // 👇 ADICIONE ESSA LINHA AQUI (fundamental para o GitHub Pages)
-    base: "/EstoqueQQ/",
-
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    }
-  };
-});
->>>>>>> b9e5aa8f7b4227e66e8d1853e5cef217ed572511
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
+})
